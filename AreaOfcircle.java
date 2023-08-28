290cb27673dc5bb28f34d608403aec513daa40c2
Ganesh Yevle <ganeshyevle9011@gmail.com>
2023-08-28 14:02:07 +0530

    All java Program Files Added

---
+++ AreaOfcircle.java
@@ -0,0 +1,10 @@
+//Area of Circle Program
+class AreaOfcircle {
+    public static void main(String[] args) {
+        double radius = 2.5;
+        
+        double area = Math.PI * radius * radius;
+        System.out.println("The area of the circle with radius " + radius + " is: " + area);
+        
+    }
+}
