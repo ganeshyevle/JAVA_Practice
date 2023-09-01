df58b72fbb89f6faed27b940a8735846cb232167
Ganesh Yevle <ganeshyevle9011@gmail.com>
2023-09-01 15:40:46 +0530

    Guess random added

--- Numberguess.java
+++ Numberguess.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class SimpleGuessinNumberGame {
+public class Numberguess{
 
     public static void main(String[] args) {
         Random random = new Random();
